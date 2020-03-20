c8b18a40afd25266a6dc7ecfde8982117c5198df
Maanik Arya <maanik@rinnegan.co>
2020-03-21 01:24:29 +0530

    Some libraries installation like lodash

--- app.js
+++ app.js
@@ -1,94 +1,85 @@
 //jshint esversion:6
 
+// Libararies Requirements
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
-const posts = [];
 
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+// Inititaliing libraries
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
 
-let composeItem = "";
-let composePost = "";
+// Varirables use for programming
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const posts = [];
 
 
+
+
+// Get request on Home page
 app.get("/", function(req, res) {
-
   res.render("home", {
     StartingContent: homeStartingContent,
     posts: posts,
-
   });
-
 });
 
-app.get("/about", function(req, res) {
 
+// Get request on about page
+app.get("/about", function(req, res) {
   res.render("about", {
     aboutContent: aboutContent
   })
 });
 
+//  Get request on contact page
 app.get("/contact", function(req, res) {
-
   res.render("contact", {
     contactContent: contactContent
   })
 });
 
+// Get request on compose page
 app.get("/compose", function(req, res) {
-
   res.render("compose");
 });
 
+// Post request on compose page
 app.post("/compose", function(req, res) {
-
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody,
   }
-
   posts.push(post);
-
   res.redirect('/');
-
-
-
 });
 
+// Routing parameters for post_title
+app.get("/posts/:postName", function(req, res) {
+  let requestedTitle = _.lowerCase(req.params.postName);
 
-app.get("/posts/:postName" , function(req , res){
-
-  let requestedTitle = req.params.postName ;
-
-  posts.forEach(function(post){
-    const storedTitle = post.title ;
-
-    if( storedTitle == requestedTitle){
+  // For loop for taking postTitle from compose page
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle == requestedTitle) {
       console.log(" match found ");
-    }
-
-    else{
+    } else {
       console.log(" match not found");
     }
+
   });
-
-
-
 });
 
+
+// Server is listening
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
